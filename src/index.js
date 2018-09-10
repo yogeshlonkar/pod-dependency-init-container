54631e65d76c09a451837a83255652cb4be2c3d9
Yogesh Lonkar <ylonkar@whitehedge.com>
2018-09-10 10:24:38 +0530

    - error logging - fixed spec loading issue for config.getInCluster()

--- src/index.js
+++ src/index.js
@@ -8,17 +8,22 @@ console.error = console.error.bind(console, '%s', CurrentDate);
 console.info('Started pod dependency lookup');
 let noOfTrys = 1;
 const checkIfPodsRunning = async () => {
-  console.info(`checking for running pods try ${noOfTrys}`);
-  const pods = await getRunningPods();
-  if (pods.length > 0) {
-    console.info(`found running pod with label(s) ${podLabels}`);
-    process.exit(0);
-  } else if (noOfTrys < maxRetry) {
-    setTimeout(checkIfPodsRunning, 1500);
-  } else {
-    console.error(`didn't find any running pod with label(s) ${podLabels} after ${noOfTrys} try(s)`);
-    process.exit(1);
+  try {
+    console.info(`checking for running pods try ${noOfTrys}`);
+    const pods = await getRunningPods();
+    if (pods.length > 0) {
+      console.info(`found running pod with label(s) ${podLabels}`);
+      process.exit(0);
+    } else if (noOfTrys < maxRetry) {
+      setTimeout(checkIfPodsRunning, 1500);
+    } else {
+      console.error(`didn't find any running pod with label(s) ${podLabels} after ${noOfTrys} try(s)`);
+      process.exit(1);
+    }
+    noOfTrys++;
+  } catch (error) {
+    console.error('couldn\'t fetch running pods');
+    console.error(error, error.message, error.stack);
   }
-  noOfTrys++;
 };
 setTimeout(checkIfPodsRunning, 1500);
