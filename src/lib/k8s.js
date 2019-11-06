b5913bad963cb5747e45647f36b5f5703dd5c77b
Chris Lamb <happyclamb@gmail.com>
2019-11-06 10:20:06 -0800

    Treat POD_LABELS as a comma seperated string of pod labels (#6)
    Ensure matched all running pod for each label provided. If the label was 'podA, podB' then it would look for a pod named 'podA, podB' instead of two pods 'podA' and 'podB'.

--- src/lib/k8s.js
+++ src/lib/k8s.js
@@ -1,22 +1,31 @@
 const Client = require('kubernetes-client').Client;
 const config = require('kubernetes-client').config;
 const { namespace, podLabels } = require('./config');
+
 const client = new Client({ config: config.getInCluster() });
-// const client = new Client({ config: config.fromKubeconfig(), version: '1.9' });
 let specLoaded = false;
 
-const getRunningPods = () => new Promise(async (resolve, reject) => {
+const getRunningPods = async () => {
   try {
     if (!specLoaded) {
       await client.loadSpec();
       specLoaded = true;
     }
-    const response = await client.api.v1.namespaces(namespace).pods.get({ qs: { labelSelector: podLabels, fieldSelector: 'status.phase=Running' } });
-    resolve(response.body.items);
+
+    const podLabelArray = podLabels.split(',').map(label => label.trim());
+    const podsRunning = await Promise.all(
+        podLabelArray.map(async podLabel => {
+          const response = await client.api.v1.namespaces(namespace).pods.get({ qs: { labelSelector: podLabel, fieldSelector: 'status.phase=Running' } });
+          return (response.body.items.length);
+        })
+    );
+
+    return(podsRunning.every(podCount => podCount > 0));
+
   } catch (error) {
-    reject(error);
+    throw (error);
   }
-});
+};
 
 module.exports = {
   getRunningPods
