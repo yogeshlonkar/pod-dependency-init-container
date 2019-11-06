ee73088af474e1962e9faa79b8be59c7cc9ea031
Chris Lamb <chris.lamb@epicure.com>
2019-11-06 08:59:14 -0800

    Treat POD_LABELS as a comma seperated string of pod labels that needs to be satisfied

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 const { getRunningPods } = require('./lib/k8s');
-const { podLabels, maxRetry, retryTimeOut } = require('./lib/config');
+const { podLabels, namespace, maxRetry, retryTimeOut } = require('./lib/config');
+
 const CurrentDate = () => { };
-CurrentDate.toString = () => `[${new Date().toISOString()}]`
+CurrentDate.toString = () => `[${new Date().toISOString()}]`;
 console.info = console.info.bind(console, '%s', CurrentDate);
 console.error = console.error.bind(console, '%s', CurrentDate);
 
-console.info('Started pod dependency lookup');
 let noOfTrys = 1;
+console.info(`Started pod dependency lookup, maxRetries ${maxRetry} with retryTimeOut ${retryTimeOut}`);
+console.info(`Using namespace '${namespace}' and looking for podLabels ${podLabels}`);
 const checkIfPodsRunning = async () => {
   try {
-    console.info(`checking for running pods try ${noOfTrys}`);
-    const pods = await getRunningPods();
-    if (pods.length > 0) {
-      console.info(`found running pod with label(s) ${podLabels}`);
+    console.info(`Checking for running pods try ${noOfTrys}`);
+    const podsRunning = await getRunningPods();
+    if (podsRunning === true) {
+      console.info(`Found running pod with label(s) ${podLabels}`);
       process.exit(0);
     } else if (noOfTrys < maxRetry) {
       setTimeout(checkIfPodsRunning, retryTimeOut);
     } else {
-      console.error(`didn't find any running pod with label(s) ${podLabels} after ${noOfTrys} try(s)`);
+      console.error(`Didn't find any running pod with label(s) ${podLabels} after ${noOfTrys} try(s)`);
       process.exit(1);
     }
     noOfTrys++;
   } catch (error) {
-    console.error('couldn\'t fetch running pods');
+    console.error(`Couldn't fetch running pods`);
     console.error(error, error.message, error.stack);
     process.exit(1);
   }
